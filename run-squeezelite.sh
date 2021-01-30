2a450e6af43bcd1452e3d9a89235bfbea852ab99
GioF71 <giovanni.fulco@gmail.com>
2021-01-30 10:25:09 +0100

    script upd

--- run-squeezelite.sh
+++ run-squeezelite.sh
@@ -5,4 +5,4 @@ sed -i 's/SQUEEZELITE_NAME/'"$SQUEEZELITE_NAME"'/g' /etc/default/squeezelite
 
 cat /etc/default/squeezelite
 
-/usr/bin/squeezelite
+/usr/bin/squeezelite -o $SQUEEZELITE_AUDIO_DEVICE -C 2

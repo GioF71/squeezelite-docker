fccc3d76501b2fc9b8ed7de99d8b69fdcd012278
GioF71 <giovanni.fulco@gmail.com>
2021-02-11 12:17:53 +0100

    Support for specified server

--- run-squeezelite.sh
+++ run-squeezelite.sh
@@ -4,8 +4,17 @@ echo "About to sleep for $STARTUP_DELAY_SEC second(s)"
 sleep $STARTUP_DELAY_SEC
 echo "Rise and shine!"
 
-/usr/bin/squeezelite \
-  -n $SQUEEZELITE_NAME \
-  -o $SQUEEZELITE_AUDIO_DEVICE \
-  -C $SQUEEZELITE_TIMEOUT \
-  -D $SQUEEZELITE_DELAY
+if [ "$SQUEEZELITE_SPECIFY_SERVER" == "yes" ]; then \
+  /usr/bin/squeezelite \
+    -s $SQUEEZELITE_SERVER_PORT \
+    -n $SQUEEZELITE_NAME \
+    -o $SQUEEZELITE_AUDIO_DEVICE \
+    -C $SQUEEZELITE_TIMEOUT \
+    -D $SQUEEZELITE_DELAY; \
+else \
+  /usr/bin/squeezelite \
+    -n $SQUEEZELITE_NAME \
+    -o $SQUEEZELITE_AUDIO_DEVICE \
+    -C $SQUEEZELITE_TIMEOUT \
+    -D $SQUEEZELITE_DELAY; \
+fi

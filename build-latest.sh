701223abc067a455aa6b0922b0f8a1227d99a517
GioF_71 <giovanni.fulco@gmail.com>
2022-01-22 01:42:17 +0100

    fixed build-latest

--- build-latest.sh
+++ build-latest.sh
@@ -5,5 +5,5 @@ docker build \
     --platform linux/arm/v7,linux/arm64/v8,linux/amd64 
     --build-arg BASE_IMAGE=bulleye \
     --build-arg DOWNLOAD_FROM_SOURCEFORGE=Y
-    --tag giof71/squeezelite:stable \
+    --tag giof71/squeezelite:latest \
     .

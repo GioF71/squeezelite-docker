6d8407d370d6db6f1e63fd03165eac4e56e5e83a
GioF_71 <giovanni.fulco@gmail.com>
2022-02-16 02:04:14 +0100

    variable names

--- test-delete-me/test-read.sh
+++ test-delete-me/test-read.sh
@@ -7,11 +7,10 @@ while IFS= read -r line
 do
   OLD_IFS=$IFS
   IFS='='
-  read -a strarr <<< "$line"
-
+  read -a line_array <<< "$line"
   if [ -n "$line" ]; then
-    key=${strarr[0]}
-    value=${strarr[1]}
+    key=${line_array[0]}
+    value=${line_array[1]}
     key=`echo "$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'<<<"${key}")"`
     value=`echo "$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'<<<"${value}")"`
     if [[ -n "$key" && -n "$value" ]]; then 
@@ -22,20 +21,4 @@ do
     echo "<<<EMPTY_LINE>>>"
   fi
 done < "$input"
-
-
-
-
-
-
-#while IFS= read -r line; do
-#  printf '%s\n' "$line"
-#  OLD_IFS=$IFS 
-#  IFS='='
-#  pair=$line
-#  echo $pair[0]  
-# #key="$(cut -d '=' -b1 <<< $line)"
-#  IFS=$OLD_IFS
-#done < presets.conf
-
 
